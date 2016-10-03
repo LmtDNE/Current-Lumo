bf5600dc14e8ccea6714dae1b1d01746d9577d6d
Dion <Dion.Fulwood@gmail.com>
2016-10-02 17:56:22 -0700

    Initial configuration of client side files

---
+++ client/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <section>
+          <h1>This App is for currency conversion!</h1>
+        </section>
+      );
+  }
+}
+
+export default App;
