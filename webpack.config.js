b66a04166054dbd366a32889890de5f62fc106d9
Dion <Dion.Fulwood@gmail.com>
2016-10-02 17:28:04 -0700

    Initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './index.js'
+  ],
+
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.css?/,
+      loader: 'style!css'
+    },
+    {
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/',
+      query: {
+          presets: ['es2015', 'react']
+        },
+    {
+      test: /\.scss?/,
+      loaders: ['style', 'css', 'postcss', 'sass']
+    }
+    }]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devServer: {
+    hot: true,
+    contentBase: './client',
+  }
+
+};
+
+
+
