54ee2fe4b3830b49a67f2958574967a535209a69
Dion <Dion.Fulwood@gmail.com>
2016-10-02 18:41:08 -0700

    Adds Component folder. Creates input component.

---
+++ client/Components/inputComponent.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>Input Amount here in USD</h3>
+        
+      </section>
+      )
+  }
+}
+
+export default Input;
