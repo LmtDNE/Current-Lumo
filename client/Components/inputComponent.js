37c727d269b7e158859ed92fbe744ca8c0ef9505
Dion <Dion.Fulwood@gmail.com>
2016-10-06 16:38:44 -0700

    Completes final styling and code clean up

--- client/Components/inputComponent.js
+++ client/Components/inputComponent.js
@@ -13,8 +13,11 @@ class Input extends React.Component {
     }
   }
 
-  inputFunction (e, dollar, cents) {
+//Functionality to convert users input amount to EUR
+  conversionHandler (e, dollar, cents) {
     e.preventDefault();
+
+    //Request api.fixer.io for latest conversion rates 
     fetch("//api.fixer.io/latest?base=USD").then((response) => {
         if (response.status >= 400) {
             throw new Error("Bad response from server");
@@ -22,57 +25,61 @@ class Input extends React.Component {
         return response.json();
     })
     .then((conversion) => {
+        //Convert amoutn and update the state with new amount
         this.setState({ currency: parseFloat(dollar.value + "."+ cents.value), converted: true, rate: conversion.rates.EUR});
     });
 
   }
-
-  reset (e) {
+//Functionality to reset state for new conversions
+  previousHelper (e) {
     e.preventDefault();
     this.setState({ currency: 0, converted: false, rate:0});
   } 
 
   render() {
     return (
-      <section className="usd-input">
-        <div className="speech-bubble">
+      <section className="usd_input">
+        <div className="speech_bubble">
           <h1>Welcome!</h1>
-          <p>I'm the Currency Robot.
-          I will convert your U.S. Dollars into Euros and display it to you!
-          Simply enter the amount you would like converted into my tummy and press enter, 
-          I'll do the rest! 
+          <p>
+            I'm the Currency Robot.
+            I will convert your U.S. Dollars into Euros and display it to you!
+            Simply enter the amount you would like converted into my tummy and click submit, 
+            I'll do the rest! 
           </p>
         </div>
         <img src="./style/images/electronic_wave.gif" id="left_eye" />
         <img src="./style/images/electronic_wave.gif" id="right_eye" />
         <div className="input_container">
-        <form id="input_form" onSubmit={e => this.inputFunction(e, this.dollar, this.cents)}>
-          <input  
-            id="dollar_amount"
-            type="number"
-            min="0"
-            step="1"
-            ref={input => this.dollar = input}
-            name="dollars"
-            placeholder="$00"/>
-          <input
-            id="cents_amount"
-            type="number"
-            maxLength="2"
-            ref={input => this.cents = input}
-            min="0"
-            max='99'
-            step='1'
-            name="cents:" 
-            placeholder=".00"/>
-          <input
-            id="submit_input"
-            type="submit"
-            name="submit_currency"/>
-        </form>
+          <form id="input_form" onSubmit={e => this.conversionHandler(e, this.dollar, this.cents)}>
+            <input  
+              id="dollar_amount"
+              type="number"
+              min="0"
+              step="1"
+              ref={input => this.dollar = input}
+              name="dollars"
+              placeholder="$00"/>
+            <input
+              id="cents_amount"
+              type="number"
+              maxLength="2"
+              ref={input => this.cents = input}
+              min="0"
+              max='99'
+              step='1'
+              name="cents:" 
+              placeholder=".00"/>
+            <input
+              id="submit_dummy"
+              type="submit"
+              name="submit_currency"/>
+          </form>
         {this.state.converted !== false ? <Output {... this.state} /> : null}
         </div>
-        <button type="button" id="reset" onClick={e => this.reset(e)}>RESET</button>
+        {this.state.converted === false ? <button type="button" id="submit_input" onClick={e => this.conversionHandler(e, this.dollar, this.cents)}>SUBMIT</button> : null}
+
+        <button type="button" id="back_button" onClick={e => this.previousHelper(e)}>BACK</button>
       </section>
 
       )
--- client/Components/outputComponent.js
+++ client/Components/outputComponent.js
@@ -3,16 +3,14 @@ import React,{Component} from 'react';
 
 
 class Output extends React.Component {
-
+//Functionality to display conversion in formatted manner
   currencyConverter() {
-    console.log("This is props", this.props);
     let convertedCurrency = this.props.currency * this.props.rate;
     return "€ " + convertedCurrency.toFixed(2);
 
   }
 
   render() {
-
     return (
         <section >
           <div id="output_currency">
