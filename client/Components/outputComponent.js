7d3d21ccb7f37a77d9d052bccc07bed7c59fa8ec
Dion <Dion.Fulwood@gmail.com>
2016-10-06 18:28:27 -0700

    Completes styling

--- client/Components/outputComponent.js
+++ client/Components/outputComponent.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react';
-
+import Loading from "react-loading";
 
 
 class Output extends React.Component {
 //Functionality to display conversion in formatted manner
   currencyConverter() {
     let convertedCurrency = this.props.currency * this.props.rate;
+    setTimeout(()=> {
+      let checker = document.getElementById("output_checkbox");
+      checker.checked = false;
+    }, 5000);
     return "€ " + convertedCurrency.toFixed(2);
 
   }
@@ -14,8 +18,8 @@ class Output extends React.Component {
     return (
         <section >
           <div id="output_currency">
-            <input type="checkbox" defaultChecked />
-            <div className="closed_door"><p>CLICK HERE!</p></div>
+            <input type="checkbox" id="output_checkbox" defaultChecked />
+            <div className="closed_door"><Loading type='bars' id="loading" color='silver' height='95' width='175' margin='10' /></div>
             <div id="output_amount">
               {this.currencyConverter()}
             </div>
--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import App from './client/app';
 
+
 ReactDom.render(<App />, document.getElementById('main'));
